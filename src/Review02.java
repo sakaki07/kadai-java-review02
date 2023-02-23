df60d1e6cb5703f1c07ddb452112a0555b6200b3
sakaki07 <cozysm@gmail.com>
2023-02-24 01:31:42 +0900

    FizzBuzz問題

---
+++ src/Review02.java
@@ -0,0 +1,26 @@
+
+public class Review02 {
+
+    public static void main(String[] args) {
+        // 1から100までの連続した整数
+        for(int i = 1; i <= 100; i++) {
+            if( i % 3 == 0 && i % 5 == 0 ) {
+                System.out.println("FizzBuzz");
+            
+            }
+            else if(i % 3 == 0 ) {
+                System.out.println("Fizz");
+                
+            }
+            else if(i % 5 == 0) {
+                System.out.println("Buzz");
+                
+                
+            }else {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
